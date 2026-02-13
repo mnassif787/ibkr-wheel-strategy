031a0da86cf5bd8d0568ff76d116c4571a63e6b9
mnassif787 <mnassif787@gmail.com>
2026-02-13 23:46:28 +0400

    fix: Simplify IB Gateway config to use hardcoded values

--- railway/start-ibgateway.sh
+++ railway/start-ibgateway.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-# Substitute environment variables in IBC config at runtime
-envsubst < /opt/ibc/config.ini.template > /opt/ibc/config.ini
-
-# Set default trading mode if not provided
-TRADING_MODE="${IBKR_TRADING_MODE:-paper}"
-
 # Run IBC with proper arguments
+# No need for envsubst since config has hardcoded values
 exec /opt/ibc/scripts/ibcstart.sh \
     10.19 \
     --gateway \
     --tws-path=/opt/ibgateway \
     --ibc-path=/opt/ibc \
     --ibc-ini=/opt/ibc/config.ini \
-    --mode="$TRADING_MODE"
+    --mode=paper
